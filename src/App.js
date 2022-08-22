9238f661ea517eab4e472e7f4974602ee47c16cb
JulianaSobaJava <julianasobajava23@gmail.com>
2022-08-22 01:49:43 +0100

    Mod: path configuration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+function App() {
+  return (
+    <div class="lua">
+    <div class="foguete">
+      <img src="./foguete.png" alt="foguete" /> 
+    </div>
+  </div>
+  );
+}
+
+export default App;
